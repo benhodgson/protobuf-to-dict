ce0353e77eb83275f9be35f00738aa25e320dff1
Ben Hodgson <ben@benhodgson.com>
2012-02-12 15:32:41 +0000

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='protobuf-to-dict',
+    description='A teeny Python library for creating Python dicts from '
+        'protocol buffers. Useful as an intermediate step before '
+        'serialisation (e.g. to JSON).',
+    version='0.0.1',
+    author='Ben Hodgson',
+    author_email='ben@benhodgson.com',
+    url='https://github.com/benhodgson/protobuf-to-dict',
+    license='Public Domain',
+    keywords=['protobuf', 'json', 'dict'],
+    install_requires=['protobuf>=2.3.0'],
+    package_dir={'':'src'},
+    py_modules=['protobuf_to_dict'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
