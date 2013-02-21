9b2f68928f153c0a843d77e6aa139b4e14dad8d7
Ben Hodgson <ben@benhodgson.com>
2013-02-21 20:32:49 +0000

    Recursive encoding of messages no longer mutates the TYPE_CALLABLE_MAP global

--- src/protobuf_to_dict.py
+++ src/protobuf_to_dict.py
@@ -33,18 +33,21 @@ def enum_label_name(field, value):
 
 
 def protobuf_to_dict(pb, type_callable_map=TYPE_CALLABLE_MAP, use_enum_labels=False):
-    # recursion!
-    type_callable_map[FieldDescriptor.TYPE_MESSAGE] = lambda pb: protobuf_to_dict(pb, type_callable_map, use_enum_labels)
     result_dict = {}
     for field, value in pb.ListFields():
         if field.type not in type_callable_map:
             raise TypeError("Field %s.%s has unrecognised type id %d" % (
                 pb.__class__.__name__, field.name, field.type))
-        type_callable = type_callable_map[field.type]
+        if field.type == FieldDescriptor.TYPE_MESSAGE:
+            # recursively encode protobuf sub-message
+            type_callable = lambda pb: protobuf_to_dict(pb,
+                type_callable_map=type_callable_map,
+                use_enum_labels=use_enum_labels)
+        else:
+            type_callable = type_callable_map[field.type]
         if use_enum_labels and field.type == FieldDescriptor.TYPE_ENUM:
             type_callable = lambda value: enum_label_name(field, value)
         if field.label == FieldDescriptor.LABEL_REPEATED:
             type_callable = repeated(type_callable)
         result_dict[field.name] = type_callable(value)
     return result_dict
-
